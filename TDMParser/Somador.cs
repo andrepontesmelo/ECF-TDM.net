e73a3b3438da230e2ce419d9b556bd2bd2bfffec
André Pontes <andrepontes@gmail.com>
2016-08-25 15:38:12 -0300

    Projeto vazio inicial de dll + testes unitários

---
+++ TDMParser/Somador.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TDMParser
+{
+	public class Somador
+	{
+		public static int Soma(int a, int b)
+		{
+			return a + b;
+		}
+	}
+}
+
---
+++ TDMParserTeste/SomadorTeste.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using TDMParser;
+
+namespace TDMParserTeste
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+			Assert.AreEqual (3, Somador.Soma (1, 2));
+		}
+	}
+}
+
