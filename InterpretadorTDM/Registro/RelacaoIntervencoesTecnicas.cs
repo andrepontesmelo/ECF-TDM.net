4f021321b5512bc2786fdfbc2737f4a83e2ff7b4
André Pontes <andrepontes@gmail.com>
2016-08-26 13:08:10 -0300

    Interpretação de relação de intervações técnicas.

---
+++ InterpretadorTDM/Registro/RelacaoIntervencoesTecnicas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace InterpretadorTDM
+{
+    public class RelacaoIntervencoesTecnicas : RegistroAbstrato
+    {
+        private DateTime dataHoraGravacao;
+        private int contadorReinicioOperacao;
+        private bool indicadorPerdaDadosMT;
+
+        public DateTime DataHoraGravacao => dataHoraGravacao;
+        public int ContadorReinicioOperacao => contadorReinicioOperacao;
+        public bool IndicadorPerdaDadosMT => indicadorPerdaDadosMT;
+
+        public RelacaoIntervencoesTecnicas(string linha) : base(linha)
+        {
+            tipoRegistro = TipoRegistro.E09_RelacaoIntervencoesTecnicas;
+            dataHoraGravacao = LerDataHora(51, 65).Value;
+            indicadorPerdaDadosMT = LerBooleano(65);
+            contadorReinicioOperacao = LerInteiro(45, 51);
+        }
+
+        public static RelacaoIntervencoesTecnicas InterpretaLinha(string linha)
+        {
+            return new RelacaoIntervencoesTecnicas(linha);
+        }
+    }
+}
+
---
+++ InterpretadorTDMTeste/RelacaoIntervencoesTecnicasTeste.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using System;
+using InterpretadorTDM;
+using System.IO;
+using System.Reflection;
+
+namespace InterpretadorTDMTeste
+{
+    [TestFixture()]
+    public class RelacaoIntervencoesTecnicasTeste
+    {
+        private static readonly string ENTRADA = 
+            "E09BE0306SC95510616430A MP-2000 TH FI       00000520141203092644S";
+
+        private RelacaoIntervencoesTecnicas lido;
+
+        [SetUp()]
+        public void Setup() 
+        {
+            lido = RelacaoIntervencoesTecnicas.InterpretaLinha(ENTRADA);
+        }
+
+        [Test()]
+        public void DeveLerTipoRegistro()
+        {
+            Assert.AreEqual(TipoRegistro.E09_RelacaoIntervencoesTecnicas, lido.TipoRegistro);
+        }
+
+        [Test()]
+        public void DeveLerNumeroFabricacao()
+        {
+            Assert.AreEqual("BE0306SC95510616430", lido.NumeroFabricacao);
+        }
+
+        [Test()]
+        public void DeveLerMFAdicional()
+        {
+            Assert.AreEqual('A', lido.MFAdicional);
+        }
+
+        [Test()]
+        public void DeveLerModeloECF()
+        {
+            Assert.AreEqual("MP-2000 TH FI       ", lido.ModeloECF);
+        }
+
+        [Test()]
+        public void DeveLerContadorReinicioOperacao()
+        {
+            Assert.AreEqual(5, lido.ContadorReinicioOperacao);
+        }
+
+        [Test()]
+        public void DeveLerDataHoraGravacao()
+        {
+            Assert.AreEqual(DateTime.Parse("2014-12-03 09:26:44"), lido.DataHoraGravacao);
+        }
+
+        [Test()]
+        public void DeveLerSimboloMoeda()
+        {
+            Assert.AreEqual(true, lido.IndicadorPerdaDadosMT);
+        }
+    }
+}
