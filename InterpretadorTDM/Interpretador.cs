6e2316206d23fe8c5576ed935bf132502fa8f15d
André Pontes <andrepontes@gmail.com>
2016-08-27 12:48:51 -0300

    Usando interpretadores no parser de arquivo tdm

--- InterpretadorTDM/Interpretador.cs
+++ InterpretadorTDM/Interpretador.cs
@@ -1,9 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using InterpretadorTDM.Registro.CNPJ;
+using System.Collections.Generic;
 using InterpretadorTDM.Registro;
+using InterpretadorTDM.Registro.CNPJ;
 using InterpretadorTDM.Registro.Relacao;
+using InterpretadorTDM.Registro.Totalizadores;
 
 namespace InterpretadorTDM
 {
@@ -12,13 +13,37 @@ namespace InterpretadorTDM
         private IdentificacaoECF identificacaoECF;
         private IdentificacaoUsuario identificacaoUsuario;
         private List<RelacaoCodificacoesGT> relacoesCodificacoesGT = new List<RelacaoCodificacoesGT>();
-        private List<RelacaoSimbolosMoeda> relacaoSimbolosMoeda = new List<RelacaoSimbolosMoeda>();
-        private List<RelacaoAlteracoesVersaoSoftwareBasico> relacaoAlteracoesVersaoSoftwareBasico = new List<RelacaoAlteracoesVersaoSoftwareBasico>();
+        private List<RelacaoSimbolosMoeda> relacoesSimbolosMoeda = new List<RelacaoSimbolosMoeda>();
+        private List<RelacaoAlteracoesVersaoSoftwareBasico> relacoesAlteracoesVersaoSoftwareBasico = new List<RelacaoAlteracoesVersaoSoftwareBasico>();
+        private List<RelacaoDispositivosMFD> relacoesDispositivosMFD = new List<RelacaoDispositivosMFD>();
+        private List<RelacaoIntervencoesTecnicas> relacoesIntervencoesTecnicas = new List<RelacaoIntervencoesTecnicas>();
+        private List<RelacaoFitasDetalheEmitidas> relacoesFitasDetalheEmitidas = new List<RelacaoFitasDetalheEmitidas>();
+        private List<PosicaoAtualTotalizadores> posicoesAtuaisTotalizadores = new List<PosicaoAtualTotalizadores>();
+        private List<RelacaoReducoesZ> relacoesReducoesZ = new List<RelacaoReducoesZ>();
+        private List<TotalizadoresParciais> totalizadoresParciais = new List<TotalizadoresParciais>();
+        private List<DemaisDocumentosEmitidos> demaisDocumentosEmitidos = new List<DemaisDocumentosEmitidos>();
+        private List<CupomFiscal> cuponsFiscais = new List<CupomFiscal>();
+        private List<DetalheCupomFiscal> detalheCuponsFiscais = new List<DetalheCupomFiscal>();
+        private List<TotalizadoresNaoFiscais> totalizadoresNaoFiscais = new List<TotalizadoresNaoFiscais>();
+        private List<MeioPagamento> meiosPagamento = new List<MeioPagamento>();
+        private List<DetalheMeioPagamento> detalhesMeioPagamento = new List<DetalheMeioPagamento>();
 
         public IdentificacaoECF IdentificacaoECF => identificacaoECF;
         public IdentificacaoUsuario IdentificacaoUsuario => identificacaoUsuario;
-        public List<RelacaoSimbolosMoeda> RelacoesSimbolosMoeda => relacaoSimbolosMoeda;
-        public List<RelacaoAlteracoesVersaoSoftwareBasico> RelacoesAlteracoesVersaoSoftwareBasico => relacaoAlteracoesVersaoSoftwareBasico;
+        public List<RelacaoSimbolosMoeda> RelacoesSimbolosMoeda => relacoesSimbolosMoeda;
+        public List<RelacaoAlteracoesVersaoSoftwareBasico> RelacoesAlteracoesVersaoSoftwareBasico => relacoesAlteracoesVersaoSoftwareBasico;
+        public List<RelacaoDispositivosMFD> RelacoesDispositivosMFD => relacoesDispositivosMFD;
+        public List<RelacaoIntervencoesTecnicas> RelacoesIntervencoesTecnicas => relacoesIntervencoesTecnicas;
+        public List<RelacaoFitasDetalheEmitidas> RelacoesFitasDetalheEmitidas => relacoesFitasDetalheEmitidas;
+        public List<PosicaoAtualTotalizadores> PosicoesAtuaisTotalizadores => posicoesAtuaisTotalizadores;
+        public List<RelacaoReducoesZ> RelacoesReducoesZ => relacoesReducoesZ;
+        public List<TotalizadoresParciais> TotalizadoresParciais => totalizadoresParciais;
+        public List<DemaisDocumentosEmitidos> DemaisDocumentosEmitidos => demaisDocumentosEmitidos;
+        public List<CupomFiscal> CuponsFiscais => cuponsFiscais;
+        public List<DetalheCupomFiscal> DetalheCuponsFiscais => detalheCuponsFiscais;
+        public List<TotalizadoresNaoFiscais> TotalizadoresNaoFiscais => totalizadoresNaoFiscais;
+        public List<MeioPagamento> MeiosPagamento => meiosPagamento;
+        public List<DetalheMeioPagamento> DetalhesMeioPagamento => detalhesMeioPagamento;
 
 		private Interpretador(string caminho)
 		{
@@ -45,15 +70,65 @@ namespace InterpretadorTDM
                     break;
 
                 case TipoRegistro.E06_RelacaoSimbolosMoeda:
-                    relacaoSimbolosMoeda.Add(RelacaoSimbolosMoeda.InterpretaLinha(linha));
+                    relacoesSimbolosMoeda.Add(RelacaoSimbolosMoeda.InterpretaLinha(linha));
                     break;
 
                 case TipoRegistro.E07_RelacaoAlteracoesVersaoSoftwareBasico:
-                    relacaoAlteracoesVersaoSoftwareBasico.Add(RelacaoAlteracoesVersaoSoftwareBasico.InterpretaLinha(linha));
+                    relacoesAlteracoesVersaoSoftwareBasico.Add(RelacaoAlteracoesVersaoSoftwareBasico.InterpretaLinha(linha));
+                    break;
+
+                case TipoRegistro.E08_RelacaoDispositivosMFD:
+                    relacoesDispositivosMFD.Add(RelacaoDispositivosMFD.InterpretaLinha(linha));
+                    break;
+
+                case TipoRegistro.E09_RelacaoIntervencoesTecnicas:
+                    relacoesIntervencoesTecnicas.Add(RelacaoIntervencoesTecnicas.InterpretaLinha(linha));
+                    break;
+
+                case TipoRegistro.E10_RelacaoFitasDetalheEmitidas:
+                    relacoesFitasDetalheEmitidas.Add(RelacaoFitasDetalheEmitidas.InterpretaLinha(linha));
+                    break;
+
+                case TipoRegistro.E11_PosicaoAtualTotalizadores:
+                    posicoesAtuaisTotalizadores.Add(PosicaoAtualTotalizadores.InterpretaLinha(linha));
+                    break;
+
+                case TipoRegistro.E12_RelacaoReducoesZ:
+                    relacoesReducoesZ.Add(RelacaoReducoesZ.InterpretaLinha(linha));
+                    break;
+
+                case TipoRegistro.E13_TotalizadoresParciais:
+                    totalizadoresParciais.Add(InterpretadorTDM.Registro.Totalizadores.TotalizadoresParciais.InterpretaLinha(linha));
+                    break;
+
+                case TipoRegistro.E14_CupomFiscal:
+                    cuponsFiscais.Add(CupomFiscal.InterpretaLinha(linha));
+                    break;
+
+                case TipoRegistro.E15_DetalheCupomFiscal:
+                    detalheCuponsFiscais.Add(DetalheCupomFiscal.InterpretaLinha(linha));
+                    break;
+
+                case TipoRegistro.E16_DemaisDocumentosEmitidos:
+                    demaisDocumentosEmitidos.Add(InterpretadorTDM.Registro.DemaisDocumentosEmitidos.InterpretaLinha(linha));
+                    break;
+
+                case TipoRegistro.E17_TotalizadoresNaoFiscais:
+                    totalizadoresNaoFiscais.Add(InterpretadorTDM.Registro.Totalizadores.TotalizadoresNaoFiscais.InterpretaLinha(linha));
+                    break;
+
+                case TipoRegistro.E18_MeioPagamento:
+                    meiosPagamento.Add(MeioPagamento.InterpretaLinha(linha));
+                    break;
+
+                case TipoRegistro.E21_DetalheMeioPagamento:
+                    detalhesMeioPagamento.Add(DetalheMeioPagamento.InterpretaLinha(linha));
                     break;
 
                 default:
-                    throw new NotImplementedException();
+                    Console.WriteLine("Não implementado: " +
+                        ObterTipoRegistro(linha).ToString());
+                    break;
             }
         }
 
