07c64388702b62395fa695d5321af29a20fc8b38
André Pontes <andrepontes@gmail.com>
2016-08-27 13:53:48 -0300

    Projeto de exemplo

---
+++ Exemplo/Exemplo.cs
@@ -0,0 +1,19 @@
+﻿using InterpretadorTDM;
+using InterpretadorTDM.Registro;
+using System;
+
+class Exemplo
+{
+    static void Main(string[] args)
+    {
+        Interpretador x = Interpretador.InterpretaArquivo(@"c:\arquivo.tdm");
+
+        DetalheCupomFiscal primeiroItem = x.DetalheCuponsFiscais[0];
+
+        Console.WriteLine(primeiroItem.Descricao);
+        Console.WriteLine(primeiroItem.Quantidade);
+        Console.WriteLine(primeiroItem.ValorTotalLiquido.ToString("C"));
+        Console.ReadLine();
+    }
+}
+
