a1e1976eed8824ce6fa94b75ae45e88d17863bcb
André Pontes <andrepontes@gmail.com>
2016-09-13 07:56:57 -0300

    Detalhamento de cupons fiscais - red

---
+++ InterpretadorTDMTeste/InterpretadorTeste.cs
@@ -0,0 +1,22 @@
+﻿using InterpretadorTDM;
+using NUnit.Framework;
+using System.IO;
+using System.Reflection;
+
+namespace InterpretadorTDMTeste
+{
+    [TestFixture()]
+    public class InterpretadorTeste
+    {
+        private static string ARQUIVO_ENTRADA = Directory.GetParent(Directory.GetParent(Path.GetDirectoryName(
+            Assembly.GetExecutingAssembly().Location)).FullName).FullName + @"\arquivo.tdm";
+
+        [Test()]
+        public void DeveDetalharCupons()
+        {
+            Interpretador interpretador = Interpretador.InterpretaArquivo(ARQUIVO_ENTRADA);
+
+            Assert.AreEqual(1, interpretador.CuponsFiscais[0].Detalhes.Count);
+        }
+    }
+}
