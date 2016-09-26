82bd0d110f80f6842ccbfb8696e6d5c41bc223cd
André Pontes <andrepontes@gmail.com>
2016-09-26 14:46:27 -0300

    Exibindo todos os cupons no exemplo

--- Exemplo/Exemplo.cs
+++ Exemplo/Exemplo.cs
@@ -6,31 +6,46 @@ class Exemplo
 {
     static void Main(string[] args)
     {
-        CupomFiscal cupom = Interpretador.InterpretaArquivo(@"c:\arquivo.tdm").CuponsFiscais[0];
+        foreach (CupomFiscal cupom in Interpretador.InterpretaArquivo(@"c:\arquivo.tdm").CuponsFiscais)
+        {
+            MostrarCupom(cupom);
+            MostrarItens(cupom);
+            MostrarPagamentos(cupom);
+        }
 
+        Console.ReadLine();
+    }
+
+    private static void MostrarPagamentos(CupomFiscal cupom)
+    {
+        Console.WriteLine(" ==== Pagamentos do Cupom Fiscal == ");
+        foreach (DetalheMeioPagamento pagamento in cupom.DetalhesMeioPagamentos)
+            Console.WriteLine(string.Format("R$ #{0} {1}", pagamento.ValorPago, pagamento.MeioPagamento));
+    }
+
+    private static void MostrarItens(CupomFiscal cupom)
+    {
+        Console.WriteLine(" ==== Itens do Cupom Fiscal == ");
+        foreach (DetalheCupomFiscal detalhe in cupom.Detalhes)
+        {
+            Console.WriteLine(string.Format("Item #{0}", detalhe.NumeroItem));
+            Console.WriteLine(string.Format("Código: {0}", detalhe.CodigoProdutoOuServico));
+            Console.WriteLine(string.Format("Unidade: {0}", detalhe.Unidade));
+            Console.WriteLine(string.Format("Descrição: {0}", detalhe.Descricao.Trim()));
+            Console.WriteLine(string.Format("Qtd: {0}", detalhe.Quantidade));
+            Console.WriteLine(string.Format("Valor Unitário: {0}", detalhe.ValorUnitario.ToString("C")));
+            Console.WriteLine(string.Format("Valor Total: {0}", detalhe.ValorTotalLiquido.ToString("C")));
+        }
+        Console.WriteLine();
+    }
+
+    private static void MostrarCupom(CupomFiscal cupom)
+    {
         Console.WriteLine(string.Format(" ==== Cupom Fiscal Código {0} ==== ", cupom.COO));
         Console.WriteLine(string.Format("Data Emissão: {0}", cupom.DataInicioEmissao));
         Console.WriteLine(string.Format("Cupom cancelado: {0}", cupom.IndicadorCancelamento));
         Console.WriteLine(cupom.ValorTotalLiquido.ToString("C"));
-
-        Console.WriteLine(" ==== Itens do Cupom Fiscal == ");
-
-        foreach (DetalheCupomFiscal detalhe in cupom.Detalhes)
-        {
-            Console.WriteLine(string.Format("Item #{0}", detalhe.NumeroItem));
-            Console.WriteLine(detalhe.CodigoProdutoOuServico);
-            Console.WriteLine(detalhe.Unidade);
-            Console.WriteLine(detalhe.Descricao);
-            Console.WriteLine(detalhe.Quantidade);
-            Console.WriteLine(detalhe.ValorUnitario.ToString("C"));
-            Console.WriteLine(detalhe.ValorTotalLiquido.ToString("C"));
-        }
-
-        Console.WriteLine(" ==== Pagamentos do Cupom Fiscal == ");
-        foreach (DetalheMeioPagamento pagamento in cupom.DetalhesMeioPagamentos)
-            Console.WriteLine(string.Format("R$ #{0} {1}", pagamento.ValorPago, pagamento.MeioPagamento));
-
-        Console.ReadLine();
+        Console.WriteLine();
     }
 }
 
